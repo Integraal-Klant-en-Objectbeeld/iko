a6685ad0ee0a9aa6b8352032ccbe6a5486700b32
Niels Erkamp <niels.erkamp@ritense.com>
2025-03-13 16:06:17 +0100

    Working petstore.

--- src/main/kotlin/com/ritense/iko/PetStoreRoute.kt
+++ src/main/kotlin/com/ritense/iko/PetStoreRoute.kt
@@ -4,6 +4,9 @@ import org.apache.camel.builder.RouteBuilder
 
 class PetStoreRoute : RouteBuilder() {
     override fun configure() {
+        // The petstore rest endpoint will sometimes return a 404 not found, because it is a dynamic rest endpoint, so you will
+        // sometimes get a 500 error on the endpoint and sometimes it will return a json :)
+        // Just try it on http://localhost:8080/hello
         from("rest:get:hello")
             .setHeader("petId", constant(1))
             .to("petstore:getPetById")
