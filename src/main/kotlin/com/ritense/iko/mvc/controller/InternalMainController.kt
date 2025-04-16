b2647b51d0ec1dc22a39e3cab4875a691ac3e0ff
Tom Bokma <tom.bokma@ritense.com>
2025-04-16 15:41:54 +0200

    working search...

--- src/main/kotlin/com/ritense/iko/mvc/controller/InternalMainController.kt
+++ src/main/kotlin/com/ritense/iko/mvc/controller/InternalMainController.kt
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
 
-
 @Controller()
 class InternalMainController(
     private val personClientService: PersonClientService,
