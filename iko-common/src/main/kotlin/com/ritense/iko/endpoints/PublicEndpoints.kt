eb1e20ff0647fcf76bc934a5aeeb5bf999d7b54b
Tom Bokma <tom.bokma@ritense.com>
2025-08-06 11:33:29 +0200

    Check if endpoint are active if not

--- iko-common/src/main/kotlin/com/ritense/iko/endpoints/PublicEndpoints.kt
+++ iko-common/src/main/kotlin/com/ritense/iko/endpoints/PublicEndpoints.kt
@@ -2,10 +2,14 @@ package com.ritense.iko.endpoints
 
 import org.apache.camel.Exchange
 import org.apache.camel.builder.RouteBuilder
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.AccessDeniedException
 
 abstract class PublicEndpoints : RouteBuilder() {
+
+    @Autowired
+    private lateinit var endpointStatusChecker: EndpointStatusChecker
 
     fun id(uri: String, to: String) {
         rest("/endpoints$uri")
@@ -17,6 +21,14 @@ abstract class PublicEndpoints : RouteBuilder() {
             .setVariable("authorities", constant("ROLE_ENDPOINT_${to.replace("direct:", "").uppercase()}"))
             .to("direct:auth")
             .routeId("direct:${to}_api_id")
+            .process { exchange ->
+                val routeId = to.replace("direct:", "")
+                if (!endpointStatusChecker.isEndpointActive(routeId)) {
+                    exchange.message.headers[Exchange.HTTP_RESPONSE_CODE] = HttpStatus.SERVICE_UNAVAILABLE.value()
+                    exchange.message.body = "{\"error\": \"Endpoint is not active\"}"
+                    exchange.setRouteStop(true)
+                }
+            }
             .to(to)
             .marshal().json()
     }
@@ -31,6 +43,14 @@ abstract class PublicEndpoints : RouteBuilder() {
             .setVariable("authorities", constant("ROLE_ENDPOINT_${to.replace("direct:", "").uppercase()}"))
             .to("direct:auth")
             .routeId("direct:${to}_api_endpoint")
+            .process { exchange ->
+                val routeId = to.replace("direct:", "")
+                if (!endpointStatusChecker.isEndpointActive(routeId)) {
+                    exchange.message.headers[Exchange.HTTP_RESPONSE_CODE] = HttpStatus.SERVICE_UNAVAILABLE.value()
+                    exchange.message.body = "{\"error\": \"Endpoint is not active\"}"
+                    exchange.setRouteStop(true)
+                }
+            }
             .to(to)
             .marshal().json()
     }
