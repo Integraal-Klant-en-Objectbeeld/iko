9957203b35c6bb36c9096f2ff2a2c4c3e121bf85
Tom Bokma <tom.bokma@ritense.com>
2025-09-05 13:41:34 +0200

    AI generated AES solution

--- src/main/kotlin/com/ritense/iko/poc/db/ConnectorInstance.kt
+++ src/main/kotlin/com/ritense/iko/poc/db/ConnectorInstance.kt
@@ -2,6 +2,7 @@ package com.ritense.iko.poc.db
 
 import jakarta.persistence.CollectionTable
 import jakarta.persistence.Column
+import jakarta.persistence.Convert
 import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
@@ -34,6 +35,7 @@ class ConnectorInstance(
     @ElementCollection(fetch = FetchType.EAGER)
     @MapKeyColumn(name = "key")
     @Column(name = "value")
+    @Convert(attributeName = "value", converter = AesGcmStringAttributeConverter::class)
     @CollectionTable(
         name = "connector_instance_config",
         joinColumns = [JoinColumn(name = "connector_instance_id")]
