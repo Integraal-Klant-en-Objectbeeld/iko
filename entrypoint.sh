384e21d6f687ff4061f958de4a6a048f51470827
Anna Vissart <anna.vissart@ritense.com>
2025-11-20 15:55:09 +0100

    * rename ARGS to OPTS for clear distinction between java options and command arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ARGS="-XX:InitialHeapSize=1024M -XX:MinRAMPercentage=70 -XX:MaxRAMPercentage=80 -Djava.security.egd=file:/dev/./urandom"
+OPTS="-XX:InitialHeapSize=1024M -XX:MinRAMPercentage=70 -XX:MaxRAMPercentage=80 -Djava.security.egd=file:/dev/./urandom"
 JWDP="-agentlib:jdwp=transport=dt_socket,server=y,address=*:8000,suspend=n"
-java $ARGS $(if [[ "$DEBUG" == "true" ]]; then echo -n "$JWDP "; else echo -n " "; fi)-jar /app/iko.jar
+java $OPTS $(if [[ "$DEBUG" == "true" ]]; then echo -n "$JWDP "; else echo -n " "; fi)-jar /app/iko.jar
