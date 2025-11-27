13623aa4a80dfe6b1c55ab3ec36ab652ba7cd335
Anna Vissart <anna.vissart@ritense.com>
2025-11-27 15:22:50 +0100

    * remove old imports

--- src/main/kotlin/com/ritense/iko/cache/autoconfiguration/CacheConfiguration.kt
+++ src/main/kotlin/com/ritense/iko/cache/autoconfiguration/CacheConfiguration.kt
@@ -1,12 +1,7 @@
 package com.ritense.iko.cache.autoconfiguration
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.ritense.iko.aggregateddataprofile.repository.AggregatedDataProfileRepository
-import com.ritense.iko.cache.processor.AdpCacheCheckProcessor
-import com.ritense.iko.cache.processor.AdpCachePutProcessor
 import com.ritense.iko.cache.processor.IkoCacheProcessor
-import com.ritense.iko.cache.processor.RelationCacheCheckProcessor
-import com.ritense.iko.cache.processor.RelationCachePutProcessor
 import com.ritense.iko.cache.service.CacheService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
