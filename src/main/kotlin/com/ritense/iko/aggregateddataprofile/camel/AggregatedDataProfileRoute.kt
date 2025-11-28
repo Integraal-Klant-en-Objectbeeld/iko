76e209b7ca5d31f68f99d9a30c9f4646cc7e4386
Anna Vissart <anna.vissart@ritense.com>
2025-11-28 20:16:53 +0100

    * remove unnecessary variables that are now handled by the cacheable interface

--- src/main/kotlin/com/ritense/iko/aggregateddataprofile/camel/AggregatedDataProfileRoute.kt
+++ src/main/kotlin/com/ritense/iko/aggregateddataprofile/camel/AggregatedDataProfileRoute.kt
@@ -31,31 +31,6 @@ class AggregatedDataProfileRoute(
                     throw IllegalArgumentException("AggregatedDataProfile with name '$aggregatedDataProfileName' not found")
                 }
                 exchange.setVariable("aggregatedDataProfileId", aggregatedDataProfile.id)
-                exchange.setVariable("cacheEnabled", aggregatedDataProfile.aggregatedDataProfileCacheSetting.enabled)
-                exchange.setVariable("cacheTTL", aggregatedDataProfile.aggregatedDataProfileCacheSetting.timeToLive)
-
-                // TODO change after ADP params are introduced
-                // Pass on the objects downstream
-                val containerParams = emptyList<ContainerParam>()
-                val filterParams = emptyMap<String, String>()
-                val adpEndpointParameterMapping = ""
-
-                exchange.setVariable("containerParams", containerParams)
-                exchange.setVariable("filterParams", filterParams)
-                exchange.setVariable("adpEndpointParameterMapping", adpEndpointParameterMapping)
-
-                if (aggregatedDataProfile.aggregatedDataProfileCacheSetting.enabled) {
-                    val combined = objectMapper.writeValueAsString(
-                        mapOf(
-                            "aggregatedDataProfileId" to aggregatedDataProfile.id,
-                            "containerParams" to containerParams,
-                            "filterParams" to filterParams,
-                            "adpEndpointParameterMapping" to adpEndpointParameterMapping
-                        )
-                    )
-                    val cacheKey = cacheService.hashString(combined)
-                    exchange.setVariable("cacheKey", cacheKey)
-                }
             }
             .toD("direct:aggregated_data_profile_\${variable.aggregatedDataProfileId}")
     }
