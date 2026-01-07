a52efc36c29086d76fcdc48440c1b825d2c09867
Tom Bokma <tom.bokma@ritense.com>
2026-01-07 17:07:37 +0100

    Remove unused annotations and clean up `BaseIntegrationTest` class

--- src/test/kotlin/com/ritense/iko/BaseIntegrationTest.kt
+++ src/test/kotlin/com/ritense/iko/BaseIntegrationTest.kt
@@ -1,18 +1,14 @@
 package com.ritense.iko
 
 import org.junit.jupiter.api.Tag
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest(
     classes = [
         TestIkoApplication::class,
-        // ClientAutoConfigure::class, <-- place here your client auto configure classes
     ]
 )
 @ActiveProfiles("test") // Will merge test yml with main yml
-@ExtendWith(value = [SpringExtension::class])
 @Tag("integration")
 abstract class BaseIntegrationTest
