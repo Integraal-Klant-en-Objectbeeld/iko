0dfd7f66880dd559fd758f0638f6918d4bd89c57
Tom Bokma <tom.bokma@ritense.com>
2026-01-08 11:42:47 +0100

    mock Oauth for testing

--- src/test/kotlin/com/ritense/iko/BaseIntegrationTest.kt
+++ src/test/kotlin/com/ritense/iko/BaseIntegrationTest.kt
@@ -7,6 +7,7 @@ import org.springframework.test.context.ActiveProfiles
 @SpringBootTest(
     classes = [
         TestIkoApplication::class,
+        IkoTestConfiguration::class,
     ]
 )
 @ActiveProfiles("test") // Will merge test yml with main yml
--- src/test/kotlin/com/ritense/iko/TestIkoApplication.kt
+++ src/test/kotlin/com/ritense/iko/TestIkoApplication.kt
@@ -1,15 +1,26 @@
 package com.ritense.iko
 
+import org.mockito.Mockito.mock
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.jwt.JwtDecoder
 
 @SpringBootApplication
 internal class TestIkoApplication
 
 @TestConfiguration
 internal class IkoTestConfiguration {
-    // Extra Beans
+    @Bean
+    @Primary
+    fun jwtDecoder(): JwtDecoder = mock(JwtDecoder::class.java)
+
+    @Bean
+    @Primary
+    fun clientRegistrationRepository(): ClientRegistrationRepository = mock(ClientRegistrationRepository::class.java)
 }
 
 fun main(args: Array<String>) {
