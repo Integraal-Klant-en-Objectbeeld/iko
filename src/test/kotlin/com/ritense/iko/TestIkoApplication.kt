60364a5b4fce7ebfcfb2d2878e25af7a975908e1
Tom Bokma <tom.bokma@ritense.com>
2026-01-08 12:30:37 +0100

    Mock OAuth2 configuration for tests and update test application setup
    - Removed Keycloak client and registration details from `application-test.yml`.
    - Added mock OAuth2 JWT resource server configuration.
    - Excluded OAuth2 client and resource server auto-configuration in `TestIkoApplication`.

--- src/test/kotlin/com/ritense/iko/TestIkoApplication.kt
+++ src/test/kotlin/com/ritense/iko/TestIkoApplication.kt
@@ -2,6 +2,8 @@ package com.ritense.iko
 
 import org.mockito.Mockito.mock
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
@@ -9,7 +11,12 @@ import org.springframework.context.annotation.Primary
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.jwt.JwtDecoder
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [
+        OAuth2ClientAutoConfiguration::class,
+        OAuth2ResourceServerAutoConfiguration::class,
+    ],
+)
 internal class TestIkoApplication
 
 @TestConfiguration
