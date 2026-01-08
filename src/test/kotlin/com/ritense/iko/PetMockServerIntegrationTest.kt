8ee3abcbecdf2962ea61bbd4456c59bfa1fc2169
Tom Bokma <tom.bokma@ritense.com>
2026-01-08 09:25:45 +0100

    Extend `PetMockServerIntegrationTest` with `BaseIntegrationTest`

--- src/test/kotlin/com/ritense/iko/PetMockServerIntegrationTest.kt
+++ src/test/kotlin/com/ritense/iko/PetMockServerIntegrationTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
-class PetMockServerIntegrationTest {
+class PetMockServerIntegrationTest : BaseIntegrationTest() {
 
     companion object {
         @JvmStatic
