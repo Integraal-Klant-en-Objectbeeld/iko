0149c4cf764341c1558860a65bcfce4ffa751871
Tom Bokma <53471398+Tom-Ritense@users.noreply.github.com>
2026-01-14 13:45:40 +0100

    Feature/setup testing (#107)
    Initial setup of int testing

---
+++ src/test/kotlin/com/ritense/iko/BaseIntegrationTest.kt
@@ -0,0 +1,15 @@
+package com.ritense.iko
+
+import org.junit.jupiter.api.Tag
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest(
+    classes = [
+        TestIkoApplication::class,
+        IkoTestConfiguration::class,
+    ],
+)
+@ActiveProfiles("test") // Will merge test yml with main yml
+@Tag("integration")
+abstract class BaseIntegrationTest
