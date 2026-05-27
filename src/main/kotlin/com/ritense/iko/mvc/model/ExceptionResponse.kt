50c3d8480817e39b412e62d9372b9d5f38f7a7d0
Tom Bokma <tom.bokma@ritense.com>
2026-05-27 09:49:32 +0200

    fix: standardize copyright heading across all files (#264)

--- src/main/kotlin/com/ritense/iko/mvc/model/ExceptionResponse.kt
+++ src/main/kotlin/com/ritense/iko/mvc/model/ExceptionResponse.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026 Ritense BV, the Netherlands.
+ * Copyright 2015-2022 Den Haag, Ritense, Rotterdam, Utrecht, the Netherlands.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
